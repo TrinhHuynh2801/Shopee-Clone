a51ed7720fac7eaaa1c45f6c3bf0df7054ab7bc7
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-02-27 22:57:28 +0700

    setting up

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-function icecreamParlor(m, arr) {
-  // Write your code here
-  const vals = {};
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    if (vals[arr[i]]) {
-      return [vals[arr[i]], i + 1];
-    }
-    vals[m - arr[i]] = i + 1;
-  }
-}
+var plusOne = function (digits) {
+  const lastIndex = digits.length - 1;
 
-console.log(icecreamParlor(100, [5, 75, 25]));
+  if (digits[lastIndex] != 9) {
+    digits[lastIndex]++;
+  } else {
+    digits[lastIndex] = 0;
+    for (let i = lastIndex - 1; i >= 0; i--) {
+      digits[i]++;
+      if (digits[i] == 10) {
+        digits[i] = 0;
+      } else {
+        break;
+      }
+    }
+  }
+  if (digits[0] == 0) digits.unshift(1);
+  return digits;
+};
+
+console.log(plusOne([9]));
