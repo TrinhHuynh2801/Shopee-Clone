e898f2a671782aa9491ceb0f8ff8f23d0e7dbb74
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-03-01 18:02:39 +0700

    header  component

--- Shopee/src/components/Footer/Footer.tsx
+++ Shopee/src/components/Footer/Footer.tsx
@@ -1,5 +1,15 @@
-import React from 'react'
-
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className='bg-neutral-100 py-6'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='flex flex-row gap-4'>
+          <div className='basis-1/2 md:basis-1/3'>© 2024 Shopee. Tất cả các quyền được bảo lưu.</div>
+          <div className='basis-1/2 md:basis-2/3'>
+            Quốc gia & Khu vực: Singapore Indonesia Thái Lan Malaysia Việt Nam Philippines Brazil México Colombia Chile
+            Đài Loan
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
