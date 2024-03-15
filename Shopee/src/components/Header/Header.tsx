50a2050398f66f738920a6ffc71fd8dee6129c04
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-03-15 23:13:51 +0700

    popover language

--- Shopee/src/components/Header/Header.tsx
+++ Shopee/src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom'
+import Popover from '../Popover/index'
 
 export default function Header() {
   return (
     <div className='py-4 max-w-7xl w-full text-white'>
       <div className=' bg-[linear-gradient(-180deg,#f53d2d,#f63)] w-[inherit]  max-w-[inherit] fixed text-xs'>
         <div className='flex flex-row justify-end my-3 gap-1 items-center pr-20'>
-          <div className='flex items-center hover:text-gray-200 cursor-pointer'>
+          <Popover
+            className='flex items-center hover:text-gray-200 cursor-pointer'
+            renderPopover={
+              <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
+                <div className='flex flex-col py-2 pr-28 pl-3'>
+                  <button className='py-2 px-3 text-left hover:text-orange-500'>Tiếng Việt</button>
+                  <button className='mt-2 py-2 px-3 text-left hover:text-orange-500'>English</button>
+                </div>
+              </div>
+            }
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -31,7 +42,7 @@ export default function Header() {
             >
               <path strokeLinecap='round' strokeLinejoin='round' d='m19.5 8.25-7.5 7.5-7.5-7.5' />
             </svg>
-          </div>
+          </Popover>
 
           <div className='mx-5 cursor-pointer flex items-center hover:text-gray-200'>
             <img
@@ -71,6 +82,22 @@ export default function Header() {
               </button>
             </div>
           </form>
+          <Link to='/'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='w-7 h-7 ml-16'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
+              />
+            </svg>
+          </Link>
         </nav>
       </div>
     </div>
