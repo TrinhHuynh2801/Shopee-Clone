d3fe828f691efba66939ddc1f0bb11cd012ae480
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-04-08 22:00:43 +0700

    add to cart

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-var subarraysWithKDistinct = function (nums, k) {
-  let distCount = new Array(nums.length + 1).fill(0);
-  let totalCount = 0;
-  let currCount = 0;
-  let left = 0;
-  let right = 0;
-  while (right < nums.length) {
-    console.log(distCount);
-
-    if (distCount[nums[right++]]++ === 0) {
-      k--;
-    }
-    if (k < 0) {
-      --distCount[nums[left++]];
-      k++;
-      currCount = 0;
-    }
-    console.log("After " + distCount);
-
-    if (k === 0) {
-      while (distCount[nums[left]] > 1) {
-        --distCount[nums[left++]];
-        currCount++;
-      }
-      totalCount += currCount + 1;
-    }
+var countStudents = function (students, sandwiches) {
+  const count = {
+    1: 0,
+    0: 0,
+  };
+  for (const student of students) {
+    count[student]++;
   }
-  return totalCount;
+
+  for (const sandwich of sandwiches) {
+    if (--count[sandwich] < 0) return count[+!sandwich];
+  }
+  return 0;
 };
 
-console.log(subarraysWithKDistinct([1, 2, 1, 2, 3], 2));
+console.log(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]));
