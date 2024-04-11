6d3fdb62aeb01692ac5dc8e2319daf09f9f388b5
TrinhHuynh2801 <trinhhuynhthien2801@gmail.com>
2024-04-11 15:06:32 +0700

    cart ui

--- Shopee/src/pages/Cart/Cart.tsx
+++ Shopee/src/pages/Cart/Cart.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 import purchaseApi from 'src/apis/purchase.api'
+import QuantityController from 'src/components/QuantityController'
 import { purchasesStatus } from 'src/constants/purchaseStatus'
+import { formatNumberWithPeriods, generateNameId } from 'src/utils/utils'
 
 export default function Cart() {
   const { data: purchasesInCartData } = useQuery({
@@ -22,7 +25,103 @@ export default function Cart() {
                   <div className='flex-grow text-black'>sản phẩm</div>
                 </div>
               </div>
+              <div className='col-span-6'>
+                <div className='grid grid-cols-5 text-center'>
+                  <div className='col-span-2'>Đơn giá</div>
+                  <div className='col-span-1'>Số lượng</div>
+                  <div className='col-span-1'>Số tiền</div>
+                  <div className='col-span-1'>Thao tác</div>
+                </div>
+              </div>
             </div>
+            <div className='my-3 rounded-sm bg-white p-5 shadow'>
+              {purchasesInCart?.map((purchase) => (
+                <div
+                  key={purchase._id}
+                  className='mb-5 grid grid-cols-12 rounded-sm border border-gray-200 bg-white py-5 px-4 text-sm text-black first:mt-0'
+                >
+                  <div className='col-span-6 self-center'>
+                    <div className='flex'>
+                      <div className='flex flex-shrink-0 items-center justify-center pr-3'>
+                        <input type='checkbox' className='h-5 w-5 accent-shopee' />
+                      </div>
+                      <Link
+                        className='h-20 w-20 flex-shrink-0'
+                        to={`/${generateNameId({
+                          name: purchase.product.name,
+                          id: purchase.product._id
+                        })}`}
+                      >
+                        <img alt={purchase.product.name} src={purchase.product.image} />
+                      </Link>
+                      <div className='flex-grow px-2 pt-1 pb-2'>
+                        <Link
+                          to={`/${generateNameId({
+                            name: purchase.product.name,
+                            id: purchase.product._id
+                          })}`}
+                          className='line-clamp-2'
+                        >
+                          {purchase.product.name}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-span-6 self-center'>
+                    <div className='grid grid-cols-5 text-center justify-center '>
+                      <div className='col-span-2'>
+                        <div className='flex items-center justify-center'>
+                          <span className='text-gray-300 line-through'>
+                            ₫{formatNumberWithPeriods(purchase.product.price_before_discount)}
+                          </span>
+                          <span className='ml-3'>₫{formatNumberWithPeriods(purchase.product.price)}</span>
+                        </div>
+                      </div>
+                      <div className='col-span-1'>
+                        <QuantityController
+                          max={purchase.product.quantity}
+                          value={purchase.buy_count}
+                          classNameWrapper='flex items-center '
+                        />
+                      </div>
+                      <div className='col-span-1'>
+                        <span className='text-orange'>
+                          ₫{formatNumberWithPeriods(purchase.product.price * purchase.buy_count)}
+                        </span>
+                      </div>
+                      <div className='col-span-1'>
+                        <button className='bg-none text-black transition-colors hover:text-shopee'>Xóa</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className='sticky bottom-0 z-10 mt-8 flex flex-col rounded-sm border border-gray-100 bg-white p-5 shadow sm:flex-row sm:items-center'>
+          <div className='flex items-center'>
+            <div className='flex flex-shrink-0 items-center justify-center pr-3'>
+              <input type='checkbox' className='h-5 w-5 accent-orange' />
+            </div>
+            <button className='mx-3 border-none bg-none'>Chọn tất cả</button>
+            <button className='mx-3 border-none bg-none'>Xóa</button>
+          </div>
+
+          <div className='mt-5 flex flex-col sm:ml-auto sm:mt-0 sm:flex-row sm:items-center'>
+            <div>
+              <div className='flex items-center sm:justify-end'>
+                <div>Tổng thanh toán (0 sản phẩm):</div>
+                <div className='ml-2 text-2xl text-orange'>₫138000</div>
+              </div>
+              <div className='flex items-center text-sm sm:justify-end'>
+                <div className='text-gray-500'>Tiết kiệm</div>
+                <div className='ml-6 text-orange'>₫138000</div>
+              </div>
+            </div>
+            <button className='mt-5 flex h-10 w-52 items-center justify-center bg-shopee text-sm uppercase text-white hover:bg-red-600 sm:ml-4 sm:mt-0'>
+              Mua hàng
+            </button>
           </div>
         </div>
       </div>
--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var deckRevealedIncreasing = function (deck) {
-  const sortArr = deck.sort((a, b) => a - b);
-  const ans = [];
-  for (let i = deck.length - 1; i >= 0; i--) {
-    if (ans.length > 0) {
-      ans.push(ans.pop());
-    }
-    ans.unshift(sortArr[i]);
-  }
-  return ans;
-};
+var removeKdigits = function (num, k) {};
 
-console.log(deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7]));
+console.log(removeKdigits("12345", 2));
